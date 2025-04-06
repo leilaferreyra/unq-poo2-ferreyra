62bc79d490f60014403ed5f29011ba29a34858f7
leilaferreyra <ferreyraleila0@gmail.com>
2025-04-06 16:58:33 -0300

    clase punto

---
+++ src/ar/edu/unq/poo2/tp3/Punto.java
@@ -0,0 +1,34 @@
+package ar.edu.unq.poo2.tp3;
+
+public class Punto {
+	private float x;
+	private float y;
+	
+	public Punto(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setXY(float x,float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+	
+
+}
